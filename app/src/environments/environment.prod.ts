aa8c51e3570ebb0f4f3c1eacde215dfa98eb4599
unknown <tkmateka@gmail.com>
2024-02-22 22:42:08 +0200

    first commit

---
+++ app/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    "name": "prod",
+    "properties": {
+        "production": true,
+        "ssdURL": "http://localhost:8081/api/",
+        "tenantName": "neutrinos-delivery",
+        "appName": "valor-mobile",
+        "namespace": "com.neutrinos-delivery.valor-mobile",
+        "useDefaultExceptionUI": true,
+        "isIDSEnabled": "false",
+        "webAppMountpoint": "web",
+        "NGFORAGE_MOBILE_DRIVER": "INDEXED_DB"
+    }
+}
